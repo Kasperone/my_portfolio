82c23d5f99b44a10dca2c7bfc01275b806c4b774
Kasperone <dwaka87@o2.pl>
2022-05-25 11:40:38 +0200

    loader_added_to_home_and_about

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,9 @@
 // import LogoK from "../../assets/images/k-logo.png"
+import "./index.scss"
 import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
 import AnimatedLetters from "../AnimatedLetters"
-import "./index.scss"
+import Loader from "react-loaders"
 
 const Home = () => {
 	const [letterClass, setLetterClass] = useState("text-animate")
@@ -38,37 +39,40 @@ const Home = () => {
 	}, [])
 
 	return (
-		<div className='container home-page'>
-			<div className='text-zone'>
-				<h1>
-					<span className={letterClass}>H</span>
-					<span className={`${letterClass} _12`}>i</span>
-					<span className={`${letterClass} _13`}>,&nbsp;</span>
-					<br />
-					<span className={`${letterClass} _14`}>I</span>
-					<span className={`${letterClass} _15`}>'</span>
-					<span className={`${letterClass} _16`}>m&nbsp;</span>
-					<AnimatedLetters
-						letterClass={letterClass}
-						strArray={nameArray}
-						idx={15}
-					/>
-					<AnimatedLetters
-						letterClass={letterClass}
-						strArray={jobArray}
-						idx={22}
-					/>
-					{/* <img src={LogoK} alt='logo-k' /> */}
-					<br />
-				</h1>
-				<h2>
-					Front-end Developer / JavaScript / React.js
-					<Link to='/contact' className='flat-button'>
-						CONTACT ME
-					</Link>
-				</h2>
+		<>
+			<div className='container home-page'>
+				<div className='text-zone'>
+					<h1>
+						<span className={letterClass}>H</span>
+						<span className={`${letterClass} _12`}>i</span>
+						<span className={`${letterClass} _13`}>,&nbsp;</span>
+						<br />
+						<span className={`${letterClass} _14`}>I</span>
+						<span className={`${letterClass} _15`}>'</span>
+						<span className={`${letterClass} _16`}>m&nbsp;</span>
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={nameArray}
+							idx={15}
+						/>
+						<AnimatedLetters
+							letterClass={letterClass}
+							strArray={jobArray}
+							idx={22}
+						/>
+						{/* <img src={LogoK} alt='logo-k' /> */}
+						<br />
+					</h1>
+					<h2>
+						Front-end Developer / JavaScript / React.js
+						<Link to='/contact' className='flat-button'>
+							CONTACT ME
+						</Link>
+					</h2>
+				</div>
 			</div>
-		</div>
+			<Loader type='pacman' />
+		</>
 	)
 }
 
