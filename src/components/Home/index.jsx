b9563783d06af3e14c61be86634b6de25a2ad0ef
Kasperone <kosecki.kasper@gmail.com>
2022-06-12 22:35:42 +0200

    return_fix_in_components

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -27,58 +27,58 @@ const Home = () => {
     return () => clearTimeout(sphereAnimation)
   }, [])
 
-  return (
-    <>
-      `
-      {loading ? (
-        <ClipLoader color="#ffffff" loading={loading} size={150} />
-      ) : (
-        <div className="container home-page">
-          <SphereAnimation />
-          <div className="text-zone">
-            <div>
-              <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i</span>
-                <span className={`${letterClass} _13`}>,&nbsp;</span>
-              </h1>
-              <br />
-              <h1>
-                <span className={`${letterClass} _14`}>I</span>
-                <span className={`${letterClass} _15`}>'</span>
-                <span className={`${letterClass} _16`}>m&nbsp;</span>
-                <AnimatedLetters
-                  letterClass={letterClass}
-                  strArray={nameArray}
-                  idx={17}
-                />
-              </h1>
-              <h1>
-                <AnimatedLetters
-                  letterClass={letterClass}
-                  strArray={jobArray}
-                  idx={22}
-                />
-              </h1>
-              <h1>
-                <AnimatedLetters
-                  letterClass={letterClass}
-                  strArray={devArray}
-                  idx={29}
-                />
-              </h1>
-            </div>
-            <h2>
-              Front-end / React.js / JavaScript
-              {/* <Link to='/contact' className='flat-button'>
+  return loading ? (
+    <ClipLoader
+      display="flex"
+      justyfy-content="center"
+      color="#ffffff"
+      loading={loading}
+      size={150}
+    />
+  ) : (
+    <div className="container home-page">
+      <SphereAnimation />
+      <div className="text-zone">
+        <div>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <span className={`${letterClass} _13`}>,&nbsp;</span>
+          </h1>
+          <br />
+          <h1>
+            <span className={`${letterClass} _14`}>I</span>
+            <span className={`${letterClass} _15`}>'</span>
+            <span className={`${letterClass} _16`}>m&nbsp;</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={17}
+            />
+          </h1>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={devArray}
+              idx={29}
+            />
+          </h1>
+        </div>
+        <h2>
+          Front-end / React.js / JavaScript
+          {/* <Link to='/contact' className='flat-button'>
 								CONTACT ME
 							</Link> */}
-            </h2>
-          </div>
-        </div>
-      )}
-      `
-    </>
+        </h2>
+      </div>
+    </div>
   )
 }
 
