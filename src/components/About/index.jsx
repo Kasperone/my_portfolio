118927521c2fdb03e0c2c16daaf29cccd7bf7c69
Kasperone <kosecki.kasper@gmail.com>
2022-06-14 14:26:46 +0200

    added_some_text_content

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -8,7 +8,7 @@ import {
   faReact,
 } from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { ClipLoader } from "react-spinners"
+import ClipLoader from "react-spinners/ClipLoader"
 import AnimatedLetters from "../AnimatedLetters/index"
 import "./index.scss"
 
@@ -48,42 +48,21 @@ const About = () => {
           />
         </h1>
         <p>
-          I'm very ambitious front-end developer looking for a role in
-          established IT company with the opportunity to work with the latest
-          technologies on challenging and diverse projects.
+          I am a beginner front-end developer with knowledge of JavaScript,
+          HTML/CSS3 and React.js. In 2022, I graduated from CodersLab, where I
+          completed a three-month daily course "JavaScript Front-End Developer".
         </p>
         <p>
-          I'm quietly confident, naturally curious, and perpetually working on
-          improving my chops one design problem at a time.
+          Every day I expand my knowledge and skills in front-end programming
+          technologies. The next points on my road-map are technologies such as
+          React native, TypeScript, Redux, Next.js, which I gradually delve into
+          while learning React.js.
         </p>
         <p>
-          If I need to define myself in one sentence that would be a family
-          person, father of a beautiful daughter, a snowboard fanatic,
-          photography enthusiast, and tech-obsessed!!!
+          I am looking for an Internship or a Junior position where I will be
+          able to further develop my passion in the best possible way, working
+          on projects with a team of experienced programmers.
         </p>
-      </div>
-
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faJsSquare} color="#444" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faReact} color="#444" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faHtml5} color="#444" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faCss3} color="#444" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#444" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#444" />
-          </div>
-        </div>
       </div>
     </div>
   )
